ed9bc478428ed5e0c2ff3aea442354573b39fd55
fishman <cho.y.j1997@gmail.com>
2024-09-01 16:13:23 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './index.css'
+
+function App() {
+
+    return (
+        <>
+            <div className="flex justify-center items-center h-screen">
+                <div className="text-center">
+                    Test
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
+
