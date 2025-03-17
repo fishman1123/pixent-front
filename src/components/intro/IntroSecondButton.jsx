c6f2a6420004ba637d315002407aa3867b5bf4bf
fishman <cho.y.j1997@gmail.com>
2025-03-17 20:04:44 +0900

    type:[#fix] 호버링 텍스트 삭제

--- src/components/intro/IntroSecondButton.jsx
+++ src/components/intro/IntroSecondButton.jsx
@@ -2,32 +2,32 @@ import React from "react";
 import "./IntroButton.css";
 
 export const IntroSecondButton = ({
-                                      perfumeName,
-                                      mainNote,
-                                      userImageUrl,
-                                      isClicked,
-                                      onClick,
-                                  }) => {
-    return (
-        <div>
-            <button
-                className={`imageSelectButton ${isClicked ? "clicked" : ""}`}
-                onClick={onClick}
-                style={{
-                    backgroundImage: `url(${userImageUrl})`,
-                    backgroundPosition: "center",
-                    backgroundSize: "cover",
-                }}
-            >
+  perfumeName,
+  mainNote,
+  userImageUrl,
+  isClicked,
+  onClick,
+}) => {
+  return (
+    <div>
+      <button
+        className={`imageSelectButton ${isClicked ? "clicked" : ""}`}
+        onClick={onClick}
+        style={{
+          backgroundImage: `url(${userImageUrl})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      >
         <span className="mainText">
           <span className="topText">{perfumeName}</span>
           <span className="subText">{mainNote}</span>
         </span>
-                <span className="hoverText">
-          <span className="topText">{perfumeName}</span>
-          <span className="subText">{mainNote}</span>
+        <span className="hoverText">
+          <span className="topText"></span>
+          <span className="subText"></span>
         </span>
-            </button>
-        </div>
-    );
+      </button>
+    </div>
+  );
 };
