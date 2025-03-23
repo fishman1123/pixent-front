d3791baa4db8d9e6a92660957d05bb789213847e
fishman <cho.y.j1997@gmail.com>
2025-03-23 19:58:40 +0900

    type:[#feature] 뒤로가기 버튼 비활성화

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,34 @@
 import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux"; // Import useDispatch and useSelector
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate, useLocation, useMatch } from "react-router-dom";
 import { Menu } from "./navbar/Menu";
 import { TranslateButton } from "./navbar/TranslateButton";
-import { BackButton } from "./navbar/BackButton.jsx";
-import { useNavigate, useLocation } from "react-router-dom";
-import { setCurrentPage } from "../store/userSlice"; // Import the Redux action for updating currentPage
+import { BackButton } from "./navbar/BackButton";
+import { setCurrentPage } from "../store/userSlice";
 
 export const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [prevPath, setPrevPath] = useState(""); // Track the previous path locally
 
   const dispatch = useDispatch();
-  const currentPage = useSelector((state) => state.user.currentPage); // Get currentPage from Redux
+  const currentPage = useSelector((state) => state.user.currentPage);
   const navigate = useNavigate();
-  const location = useLocation(); // Hook to track location changes
+  const location = useLocation();
 
-  // Update currentPage whenever the location changes
+  const isCollectionPage = useMatch("/secured/Collection/");
+
   useEffect(() => {
-    dispatch(setCurrentPage(location.pathname)); // Dispatch the new path to Redux
-  }, [location.pathname, dispatch]);
+    setPrevPath(currentPage);
+    dispatch(setCurrentPage(location.pathname));
+  }, [location.pathname, dispatch, currentPage]);
+
+  const isComingFromFeedback = prevPath.startsWith("/feedback/");
+  const shouldShowBackButton = !isCollectionPage && !isComingFromFeedback;
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
+
   const handleRedirect = () => {
     window.location.href = "/";
   };
@@ -30,7 +37,13 @@ export const Navbar = () => {
     <div className="max-w-[480px] mx-auto">
       <div className="fixed top-0 left-0 w-full text-black z-30">
         <div className="max-w-[480px] mx-auto bg-white flex items-center justify-between p-5">
-          {currentPage === "/" ? <TranslateButton /> : <BackButton />}
+          {/* Hide back button if the previous path was /feedback/* or if on /secured/Collection/ */}
+          {currentPage === "/" ? (
+            <TranslateButton />
+          ) : shouldShowBackButton ? (
+            <BackButton />
+          ) : null}
+
           <div className="flex-1 text-[24px] font-headerTitle text-center">
             <button onClick={handleRedirect}>AC'SCENT ID</button>
           </div>
